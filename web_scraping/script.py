5a5e809fa932441facea937102e502f2c669aa13
26 <muongkimhong086@gmail.com>
2022-04-20 13:14:13 +1000

    format result for f1 calendar

--- web_scraping/script.py
+++ web_scraping/script.py
@@ -10,6 +10,7 @@ class WebScraping:
 
     def scrap_f1_calendar(self):
         results = []
+        formatted_results = []
         current_year = datetime.now().year
         default_url  = f"https://www.espn.com/f1/schedule/_/year/{current_year}"
 
@@ -25,7 +26,17 @@ class WebScraping:
             cols = [element.text.strip() for element in cols]
             results.append([element for element in cols if element])
 
-        return results
+        for result in results:
+            date = result[0]
+            object = {
+                'season': str(current_year),
+                'start_date': f"{date.split(' ')[0]} {date.split(' ')[1]}",
+                'end_date': f"{date.split(' ')[0]} {date.split(' ')[3]}",
+                'track': result[1]
+            }
+            formatted_results.append(object)
+
+        return formatted_results
 
     def scrap_f1_result(self):
         pass 
